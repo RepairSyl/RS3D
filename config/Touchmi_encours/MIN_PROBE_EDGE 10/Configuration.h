c62f8e523288a32edd16ff35555ba64282b65700
RepairSyl <contact@repairsyl.xyz>
2020-03-19 23:59:52 +0100

    touchmi in progress

--- config/Touchmi_encours/MIN_PROBE_EDGE 10/Configuration.h
+++ config/Touchmi_encours/MIN_PROBE_EDGE 10/Configuration.h
@@ -962,7 +962,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -10.5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -6, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
